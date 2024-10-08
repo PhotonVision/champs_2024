90b751cc86d013bb74bc1ad3a7acbb748fa16eff
Matt <matthew.morley.ca@gmail.com>
2024-10-07 21:21:56 -0700

    refactor

--- sim_projects/apriltag_yaw_only/meme.py
+++ sim_projects/apriltag_yaw_only/meme.py
@@ -6,47 +6,53 @@ import struct
 for file in os.listdir("logs"):
     reader = DataLogReader(f"logs/{file}")
 
-    topics: Dict[int, StartRecordData] = {}
+    startRecordMap: Dict[int, StartRecordData] = {}
 
-    messages: Dict[int, List[DataLogRecord]] = {}
+    messagesByTopic: Dict[int, List[DataLogRecord]] = {}
 
-    schemas: Dict[int, str] = {}
+    schemasByTopic: Dict[int, str] = {}
 
     for entry in reader:
         if entry.isStart():
             data = entry.getStartData()
 
-            print(f"Start record on topic {data.entry} : {data.name}")
+            # print(f"Start record on topic {data.entry} : {data.name}")
             
-            topics[data.entry] = data
-            messages[data.entry] = []
-
-            if "struct:" in data.name:
-                print("Typestring is: " + data.type)
-                print("Metadata is: " + data.metadata)
+            startRecordMap[data.entry] = data
+            messagesByTopic[data.entry] = []
 
         elif entry.isFinish():
             data = entry.getFinishEntry()
-            print(f"Finish record on topic {data}")
+            # print(f"Finish record on topic {data}")
             pass
         elif entry.isControl():
-            print(f"Control record on topic {data}")
+            # print(f"Control record on topic {data}")
             pass
         elif entry.isSetMetadata():
-            print(f"Set metadata record on topic {data}")
+            # print(f"Set metadata record on topic {data}")
             pass
         else:
-            print(f"Normal message published on topic ID {entry.getEntry()}: len {entry.getSize()}")
-            if entry.getEntry() in topics.keys():
-                messages[entry.getEntry()].append(entry)
+            # print(f"Normal message published on topic ID {entry.getEntry()}: len {entry.getSize()}")
+            if entry.getEntry() in startRecordMap.keys():
+                messagesByTopic[entry.getEntry()].append(entry)
 
-            startData = topics[entry.getEntry()]
+            startData = startRecordMap[entry.getEntry()]
+            
             if ".schema/struct:" in startData.name:
-                print(entry.getRaw())
+                print(startData.name)
+                print(entry.getRaw().decode())
 
-    print("")
-    print(f"Topics in {file}:")
-    for (key, topic) in topics.items():
-        print("  " + topic.name)
+                # huge hack
+                if "TagDetection" in startData.name:
+                    schemasByTopic[entry.getEntry()] = "<Ldddddddd"
+                if "Twist3d" in startData.name:
+                    schemasByTopic[entry.getEntry()] = "<dddddd"
+
+    
+
+    # print("")
+    # print(f"Topics in {file}:")
+    # for (key, topic) in startRecordMap.items():
+    #     print("  " + topic.name)
 
     print("=========")
